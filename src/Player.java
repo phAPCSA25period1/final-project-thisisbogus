dd8feb0650584bae910caf9ed3222b81b9c6acf8
thisisbogus <aidenhmorris09@gmail.com>
2026-05-06 09:41:35 -0700

    Added javadoc comments

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,19 @@
+/**
+ * The player constructor holds all of the information that is related directly to an individual player.
+ * This class is called to by the Round class in most places, and it needed to be consolidated to its own class for the sake of efficiency.
+ * It contains various getters and setters (taking the form of adders) to allow its variables held within the private playerObjects array to be accessed easily.
+ */
 public class Player {
 
     private Object[][] playerObjects;
 
+/**
+ * Creates the individual player with a name the user will input.
+ * This allows for distinction between players so theres no risk of confusion.
+ * <p>
+ * The constructor also initializes the playerObjects array.
+ * @param givenName the individual player's name
+ */
     public Player(String givenName)
     {
         playerObjects = new Object[3][1];
@@ -10,28 +22,55 @@ public class Player {
         playerObjects[2][0] = "";
     }
 
+/**
+ * Returns the player's name from the playerObjects array.
+ * @return      the player's name
+ */
     public String getName()
     {
         return (String) playerObjects[0][0];
     }
 
-    public void addPoints(int additionalPoints)
+/**
+ * Returns the current story from the playerObjects array.
+ * @return      the player's points
+ */
+    public int getPoints()
     {
-        playerObjects[1][0] = ((int) playerObjects[1][0]) + additionalPoints;
+        return (int) playerObjects[1][0];
     }
 
-    public void addToStory(String additionalStory)
-    {
-        playerObjects[2][0] = ((String) playerObjects[2][0]) + additionalStory;
-    }
-
+/**
+ * Returns the current story from the playerObjects array.
+ * @return      the player's story string
+ */
     public String getStory()
     {
         return (String) playerObjects[2][0];
     }
 
-    public int getPoints()
+
+
+/**
+ * Adds to the points in the playerObjects array.
+ * <p>
+ * There is no need to fully set the variable as there is no instance in the program where points is not simply added to.
+ * This minimizes any potential coding errors, though negative numbers could be input for a subtraction effect.
+ * @param additionalPoints the amount of points to be added to the players total
+ */
+    public void addPoints(int additionalPoints)
     {
-        return (int) playerObjects[1][0];
+        playerObjects[1][0] = ((int) playerObjects[1][0]) + additionalPoints;
     }
+
+/**
+ * Pieces the two pieces of the story together, from one round to the next.
+ * Accesses the story string in the playerObjects array.
+ * @param additionalStory the string of words that will be concatenated with the existing string
+ */
+    public void addToStory(String additionalStory)
+    {
+        playerObjects[2][0] = ((String) playerObjects[2][0]) + additionalStory;
+    }
+
 }
--- src/Round.java
+++ src/Round.java
@@ -1,13 +1,24 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * Houses the entire round to be played with the amount of rounds and a list of players using the Player class as private variables to be set in the constructor.
+ */
 public class Round {
 
     private int rounds;
     public int round = 1;
     private Player[] players;
 
-
+/**
+ * Sets the amount of rounds in the game as well as creating the players that the user will input.
+ * Initializes the players array.
+ * <p>
+ * Loops through the newly initialized player array and initializes the players with names input with the scanner.
+ * @param roundCount the amount of rounds that will be played in the game
+ * @param playerCount the amount of players that will play the game
+ * @param userText the scanner that will be used to input text
+ */
     public Round(int roundCount, int playerCount, Scanner userText)
     {
         rounds = roundCount;
@@ -18,11 +29,19 @@ public class Round {
             players[i] = new Player(userText.next());
         }
 
-        Object testObject = players[0];
-        System.out.println(((Player) testObject).getName() + " DID IT WORK");
-
     }
 
+/**
+ * A bit of a large method.
+ * If I could do it again I'd probably seperate it into multiple for better readibility.
+ * <p>
+ * The core of the actual game, creating the collaborative stories.
+ * It takes in user input each round, prompting them to write a piece of a story.
+ * Once the player has entered their input, it will cut it in half, unless it's the end of the story.
+ * <p>
+ * It will then print the full stories, and ask each players to give it 1-10 points.
+ * Finally, it loops through the players to check who has the greatest amount of points, and declares them the winner.
+ */
     public void PlayRound()
     {
         Scanner input = new Scanner(System.in);
@@ -95,18 +114,18 @@ public class Round {
                 }
             }
        }
-            int mostPoints = 0;
-            String winner = "";
-            for (Player item : players)
+        int mostPoints = 0;
+        String winner = "";
+        for (Player item : players)
+        {
+            System.out.println(item.getName() + " has " + item.getPoints() + " points!");
+            if (item.getPoints() > mostPoints)
             {
-                System.out.println(item.getName() + " has " + item.getPoints() + " points!");
-                if (item.getPoints() > mostPoints)
-                {
-                    mostPoints = item.getPoints();
-                    winner = item.getName();
-                }
+                mostPoints = item.getPoints();
+                winner = item.getName();
             }
-            System.out.println(winner + " has won the game!");
+        }
+        System.out.println(winner + " has won the game!");
         input.close();
     }
 
